c3003886a764fd9e63bf3d33c882ed04475790a7
Piotr Ciepliński <pcieplinski@hpc.kip.polsl.pl>
2024-09-14 16:13:17 +0200

    Wyznaczanie powierzchni pod krzywmi wynikow

--- research/basics/utils.py
+++ research/basics/utils.py
@@ -17,4 +17,10 @@ def savePlotFig(config, alg: str, fig_name: str):
     plt.savefig(os.path.join(figs_dir, fig_name))
 
 def getResultsFilePath(config, alg: str, agg: bool, meta: bool):
-    return os.path.join(config["results_dir"], alg, f"results_{'Meta' if meta else ''}IS_{'agg_' if agg else ''}{config['result_postfix']}.csv")
+    return getBaseResultsFilePath(config, alg, f"results_{'Meta' if meta else ''}IS_{'agg_' if agg else ''}{config['result_postfix'] if meta else config['IS_result_postfix']}.csv")
+
+def getSurfacesResultsFilePath(config, alg: str):
+    return getBaseResultsFilePath(config, alg, f"results_surfaces_{config['result_postfix']}.csv")
+
+def getBaseResultsFilePath(config, alg: str, filename: str):
+    return os.path.join(config["results_dir"], alg, filename)
--- research/results_surface_calculator.py
+++ research/results_surface_calculator.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from research.basics.utils import getResultsFilePath, loadConfig
+from research.basics.utils import getResultsFilePath, getSurfacesResultsFilePath, loadConfig
 
 config = loadConfig()
 
@@ -8,18 +8,25 @@ dfs = []
 for alg in config['models']:
     df = pd.read_csv(getResultsFilePath(config, alg, True, True),header=[0,1])
     df_ref = pd.read_csv(getResultsFilePath(config, alg, True, False),header=[0,1])
-    dfs.append((df, df_ref))
+    dfs.append((df, df_ref, alg))
+
+
 
 for t in dfs:
+    results = []
     datasets = np.unique(df['name'])
     for i,ds in enumerate(datasets):
         df = t[0]
         df_ref = t[1]
         df_tmp = df[(df['name']==ds).values]
+        df_tmp_red_rate = df_tmp[('red_rate','mean')].values
+        df_tmp_acc = df_tmp[('acc','mean')].values
         df_ref_tmp = df_ref[(df_ref['name']==ds).values]
-        print(ds)
-        powierzchniaWzorca = df_ref_tmp[('red_rate', 'mean')] * df_ref_tmp[('acc', 'mean')]
-        print(f"Powierzchnia wzorca {powierzchniaWzorca}")
-        
-        #print(df_tmp[('red_rate','mean')])
-        #print(df_tmp[('acc','mean')])
+
+        powierzchniaWzorca = (df_ref_tmp[('red_rate', 'mean')] * df_ref_tmp[('acc', 'mean')]).sum()
+        area = np.trapz(df_tmp_acc, x=df_tmp_red_rate)
+        area += df_tmp_red_rate[0] * df_tmp_acc[0] #uwzględnienie w powierzchni pod krzywą prostokąta, którego krawędzie wzynacza oś x i oś y do pierwszego punktu pomiarowego
+        results.append([ds, powierzchniaWzorca, area])
+
+    results_df = pd.DataFrame(results, columns=['dataset', 'IS surface', 'MetaIS surface'])
+    results_df.to_csv(getSurfacesResultsFilePath(config, t[2]), index=False)
