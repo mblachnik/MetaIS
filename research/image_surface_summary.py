2e9bde639edba8c8e3d6993f54a91ca9c0269f2d
Piotr Ciepliński <pcieplinski@hpc.kip.polsl.pl>
2024-09-26 17:16:26 +0200

    dodanie opisów osi oraz zapis do pliku

--- research/image_surface_summary.py
+++ research/image_surface_summary.py
@@ -34,12 +34,37 @@ ref_color = 'g'
 plt.plot(X,Y,color=meta_color,label=ds,marker='*')
 plt.plot(X_REF, Y_REF,color=ref_color,marker='o')
 
+plt.xlabel('Compression [%]')
+plt.ylabel('Accuracy [%]') 
+
 plt.axhline(0, color='black',linewidth=1)
 plt.axvline(0, color='black',linewidth=1)
 
+plt.gca().spines['top'].set_color('none')
+plt.gca().spines['right'].set_color('none')
+plt.gca().spines['left'].set_color('none')
+plt.gca().spines['bottom'].set_color('none')
+
 plt.fill_between(X, Y, color='white', edgecolor=meta_color, hatch='//', alpha=0.5)
 plt.fill_between(X_REF, Y_REF, color='white', edgecolor=ref_color, hatch='\\\\',alpha=0.5)
+
+figs_dir = os.path.join(f"{config['results_dir']}", "figure1.png")
+plt.savefig(figs_dir)
 plt.show()
+
+plt.plot(X,Y,color=meta_color,label=ds,marker='*')
+plt.plot(X_REF, Y_REF,color=ref_color,marker='o')
+
+plt.xlabel('Compression [%]')
+plt.ylabel('Accuracy [%]') 
+
+plt.axhline(0, color='black',linewidth=1)
+plt.axvline(0, color='black',linewidth=1)
+
+plt.gca().spines['top'].set_color('none')
+plt.gca().spines['right'].set_color('none')
+plt.gca().spines['left'].set_color('none')
+plt.gca().spines['bottom'].set_color('none')
 
 interp_func = interp1d(X, Y, kind='linear', fill_value='extrapolate')
 x_target = X_REF.iloc[1]
@@ -49,15 +74,10 @@ Y_filtered = Y.head(len(X_filtered))
 X_filtered.iloc[-1] = x_target
 Y_filtered.iloc[-1] = y_target
 
-
-plt.plot(X,Y,color=meta_color,label=ds,marker='*')
-plt.plot(X_REF, Y_REF,color=ref_color,marker='o')
-
-plt.axhline(0, color='black',linewidth=1)
-plt.axvline(0, color='black',linewidth=1)
-
 plt.fill_between(X_filtered, Y_filtered, color='white', edgecolor=meta_color, hatch='//', alpha=0.5)
 plt.fill_between(X_REF, Y_REF, color='white', edgecolor=ref_color, hatch='\\\\',alpha=0.5)
+figs_dir = os.path.join(f"{config['results_dir']}", "figure2.png")
+plt.savefig(figs_dir)
 plt.show()
 
 
